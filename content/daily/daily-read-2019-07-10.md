ff77ff3ace90ae65d8f22e0fac93d2bf3328f1d8
tommykwan <tommy.kwan@acsdev.net>
2019-07-10 16:19:04 +0800

    update blog setting Wed Jul 10 16:19:04 HKT 2019

--- content/daily/daily-read-2019-07-10.md
+++
@@ -1,6 +0,0 @@
----
-title: "Daily read 2019-07-10"
-date: 2019-07-10T18:00:00+08:00
----
-
-- [xdite 价值投资密笈](https://github.com/xdite/value-investing-newbie)
--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,14 @@
 echo -e "\033[0;32mDeploying core to GitHub...\033[0m"
 
 git add .
-
-git commit -m "udpate blog setting `date`"
-
+git commit -m "update blog setting `date`"
 git push origin master:core
 
 echo -e "\033[0;32mDeploying posts to GitHub...\033[0m"
 
 # Build the project.
 hugo -t hyde
-
-# Go To Public folder
 cd public
-# Add changes to git.
 git add .
 
 # Commit changes.
@@ -24,9 +19,5 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
 cd ..
