0837aa2cdd47daf73dcafb48f5467c1e4ceae282
kyktommy <tommy.kwan@acsdev.net>
2019-12-19 00:08:07 +0800

    update blog setting Thu Dec 19 00:08:07 HKT 2019

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying core to GitHub...\033[0m"
-
-git add .
-git commit -m "update blog setting `date`"
-git push origin core
-
 echo -e "\033[0;32mDeploying posts to GitHub...\033[0m"
 
 # Build the project.
@@ -21,3 +15,9 @@ fi
 git commit -m "$msg"
 git push origin master
 cd ..
+
+echo -e "\033[0;32mDeploying core to GitHub...\033[0m"
+
+git add .
+git commit -m "update blog setting `date`"
+git push origin core
